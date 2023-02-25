cb82f7eddbf75f107605b44be38f941c80b6d601
Sahil Bose <bosesahil44@gmail.com>
2023-02-25 17:44:23 +0530

    Login and Register Frontend work

--- frontend/src/app/components/Header.jsx
+++ frontend/src/app/components/Header.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import {FaSignInAlt, FaSignOutAlt , FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from 'concurrently'
 
 
 function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout =() =>{
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
   return (
     <header className='header'>
         <div className="logo">
             <Link to='/'>Goal Setter</Link>
         </div>
         <ul>
-            <li>
+            {user ? (<li>
+                <button className='btn' onClick={onLogout}>
+                    <FaSignOutAlt/>Logout
+                </button>
+            </li>) : (<><li>
                 <Link to='/login'>
                     <FaSignInAlt/>Login
                 </Link>
@@ -20,6 +36,8 @@ function Header() {
                     <FaUser/>Register
                 </Link>
             </li>
+            </>) }
+            
         </ul>
     </header>
   )
